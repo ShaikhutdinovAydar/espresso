5096e0b92e7eb824d6c29e3a9eb3ce2f2a83ec0d
Almet-Shaikhutdinov <rijic03@mail.ru>
2019-11-12 19:44:51 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow,QTableWidgetItem
-from ui_1 import Ui_Form
-import csv
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
-class MyWidget(QMainWindow, Ui_Form):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.loadTable('Data.csv')
-
-    def loadTable(self):
-        
-        self.tableWidget.setColumnCount(len(title))
-        self.tableWidget.setHorizontalHeaderLabels(title)
+        uic.loadUi('main.ui', self)
+        # Подключение к БД
+        con = sqlite3.connect("coffee.sqlite")
+        # Создание курсора
+        cur = con.cursor()
+        # Выполнение запроса и получение всех результатов
+        self.result = cur.execute("SELECT * FROM coffees").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["id", "title", "degree of roasting", "ground in grains", "the description of the taste", "cost",
+             "the amount of taste"])
         self.tableWidget.setRowCount(0)
-        for i, row in enumerate(reader):
+        for i, row in enumerate(self.result):
             self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
             for j, elem in enumerate(row):
-                self.tableWidget.setItem(i, j, QTableWidgetItem(elem))
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
+        con.commit()
+        con.close()
 
 
-app = QApplication(sys.argv)
-ex = MyWidget()
-ex.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
