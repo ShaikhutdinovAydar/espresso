8db82703744964e152307f8eb0a85d6b0d616223
Almet-Shaikhutdinov <rijic03@mail.ru>
2019-11-12 19:28:04 +0300

    initial commit

---
+++ flag.py
@@ -0,0 +1,35 @@
+import sys
+import sqlite3
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('Filmateka.ui', self)
+        # Подключение к БД
+        con = sqlite3.connect("coffee.sqlite")
+        # Создание курсора
+        cur = con.cursor()
+        # Выполнение запроса и получение всех результатов
+        self.result = cur.execute("SELECT * FROM coffees").fetchall()
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["id", "title", "degree of roasting", "ground in grains", "the description of the taste", "cost",
+             "the amount of taste"])
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(self.result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.tableWidget.resizeColumnsToContents()
+        con.commit()
+        con.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -1,43 +1,28 @@
 import sys
-import random
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow,QTableWidgetItem
+from ui_1 import Ui_Form
+import csv
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        self.c = 0
-        self.setGeometry(0, 0, 600, 600)
-        self.setWindowTitle('Git и желтые окружности')
-        self.btn = QPushButton('Создать', self)
-        self.btn.clicked.connect(self.up)
+        self.setupUi(self)
+        self.loadTable('Data.csv')
 
-    def up(self):
-        self.c = 1
-        self.update()
-
-    def paintEvent(self, event):
-        if self.c != 0:
-            self.qp = QPainter()
-            self.qp.begin(self)
-            self.create_cyrcle()
-            self.qp.end()
-
-    def create_cyrcle(self):
-        for i in range(10):
-            rad = random.randint(0, 300)
-            x = random.randint(0, 500)
-            y = random.randint(0, 500)
-            r = random.randint(0, 255)
-            g = random.randint(0, 255)
-            b = random.randint(0, 255)
-            self.qp.setBrush(QColor(r, g, b))
-            self.qp.drawEllipse(x, y, rad, rad)
+    def loadTable(self):
+        
+        self.tableWidget.setColumnCount(len(title))
+        self.tableWidget.setHorizontalHeaderLabels(title)
+        self.tableWidget.setRowCount(0)
+        for i, row in enumerate(reader):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(elem))
+        self.tableWidget.resizeColumnsToContents()
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWidget()
-    ex.show()
-    sys.exit(app.exec_())
+app = QApplication(sys.argv)
+ex = MyWidget()
+ex.show()
+sys.exit(app.exec_())
